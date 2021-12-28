521fef3d1e04fbeb4149f966a6904e570e57dac9
fearm3 <ibraaa1628@gmail.com>
2021-12-28 19:13:26 +0300

    add router

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+
+
+const App = () => {
+    return (
+        <div className="App">
+            App
+        </div>
+    )
+}
+
+export default App
---
+++ src/router/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Main from "../pages/Main";
+import Register from "../pages/Register";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default Router;
