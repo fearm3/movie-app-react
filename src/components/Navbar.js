a86cf9651ba0df504ac43d91ee72e93aa6de3776
fearm3 <ibraaa1628@gmail.com>
2021-12-28 21:29:58 +0300

    add register

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { signOut } from "firebase/auth";
+import { auth } from "../auth/firebase-config";
 
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const navigate = useNavigate();
+  const { currentUser } = useContext(AuthContext);
 
-export default Navbar
+  const signOutFunc = async () => {
+    await signOut(auth);
+  };
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand " href="/" style={{ color: "white" }}>
+            React Movie App
+          </a>
+          <div className="buttons">
+            {currentUser ? (
+              <h3>{currentUser.displayName}</h3>
+            ) : (
+              <button
+                type="button"
+                className="ms-2 btn btn-outline-light"
+                onClick={() => navigate("/login")}
+              >
+                Login
+              </button>
+            )}
+            {currentUser ? (
+              <button
+                type="button"
+                className="ms-2 btn btn-outline-light"
+                onClick={() => signOutFunc()}
+              >
+                Logout
+              </button>
+            ) : (
+              <button
+                type="button"
+                className="ms-2 btn btn-outline-light"
+                onClick={() => navigate("/register")}
+              >
+                Register
+              </button>
+            )}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/router/Router.js
+++ src/router/Router.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
 import Login from "../pages/Login";
 import Main from "../pages/Main";
 import Register from "../pages/Register";
@@ -7,6 +8,7 @@ import Register from "../pages/Register";
 const AppRouter = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
