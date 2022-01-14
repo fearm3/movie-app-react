53687ed4c34dadd71159574dabef5690c1e910b4
fearm3 <ibraaa1628@gmail.com>
2022-01-14 18:36:19 +0300

    add ap

--- src/auth/firebase-config.js
+++ src/auth/firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Add firebase keys in .env files
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyBvRe85kKWp3Y96P3Cp1ovu8wNmw_zpVSo",
+  authDomain: "movie-app-github.firebaseapp.com",
+  projectId: "movie-app-github",
+  storageBucket: "movie-app-github.appspot.com ",
+  messagingSenderId: "658320230436",
+  appId: "1:658320230436:web:32fa9cd8b65e391a5c30b1",
 };
 
 const app = initializeApp(firebaseConfig);
